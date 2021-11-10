601d68b1d471efa96578c28921214ad9615f984b
Ashish Rai <ashishrai96@yahoo.com>
2021-11-10 14:16:08 +0530

    Header design added

--- IHFF-UI/src/app/app-routing.module.ts
+++ IHFF-UI/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AudioTranscriptComponent } from './components/audio-transcript/audio-transcript.component';
+import { SurveyFormComponent } from './components/survey-form/survey-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'form', component: SurveyFormComponent },
+  { path: 'transcript', component: AudioTranscriptComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- IHFF-UI/src/app/app.module.ts
+++ IHFF-UI/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared/shared.module';
+import { SharedModule } from './shared/shared.module';
+import { SurveyFormComponent } from './components/survey-form/survey-form.component';
+import { AudioTranscriptComponent } from './components/audio-transcript/audio-transcript.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    SurveyFormComponent,
+    AudioTranscriptComponent,
   ],
   imports: [
     BrowserModule,
