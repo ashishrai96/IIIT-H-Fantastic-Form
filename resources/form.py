eec4c222735e29a2d6349d73b24f77290e053f21
Nilesh Jha <nileshjha412@gmail.com>
2021-11-14 16:11:09 +0530

    Add resources for form, questions & users

---
+++ resources/form.py
@@ -0,0 +1,28 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.form import FormModel
+
+
+class Form(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('url',
+                        type=str,
+                        required=True,
+                        help="Every question needs a URL."
+                        )
+    parser.add_argument('creator_id',
+                        type=int,
+                        required=True,
+                        help="Every question needs a creator_id."
+                        )
+
+    def post(self):
+        data = Form.parser.parse_args()
+        print(data['url'])
+        form = FormModel(data['url'],data['creator_id'])
+        try:
+            form.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the form."}, 500
+        return form.json(), 201
+
