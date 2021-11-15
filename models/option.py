bdcec6d2c4faa32e792e76e106a3ad7f4c078d9a
shlok299 <shlok2999@gmail.com>
2021-11-15 13:01:16 +0530

    Added some classmethods

--- models/option.py
+++ models/option.py
@@ -6,3 +6,8 @@ class OptionModel(db.Model):
     ques_id = db.Column(db.Integer,db.ForeignKey('question.id',ondelete="CASCADE"), nullable=False)
     option = db.Column(db.String(10), nullable=False)
     description= db.Column(db.String(100), nullable=False)
+
+    def __init__(self,ques_id,option,description):
+        self.ques_id = ques_id
+        self.option = option
+        self.description= description
