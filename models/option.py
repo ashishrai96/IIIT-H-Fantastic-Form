e723dc6f83298c99c516ff4ebe344b74df9d2a5e
shlok299 <shlok2999@gmail.com>
2021-11-18 15:51:03 +0530

    Changed the attribute detaails

--- models/option.py
+++ models/option.py
@@ -4,10 +4,9 @@ class OptionModel(db.Model):
     __tablename__ = 'option'
     id= db.Column(db.Integer,primary_key=True)
     ques_id = db.Column(db.Integer,db.ForeignKey('question.id',ondelete="CASCADE"), nullable=False)
-    option = db.Column(db.String(10), nullable=False)
+    option = db.Column(db.String(10))
     description= db.Column(db.String(100), nullable=False)
 
-    def __init__(self,ques_id,option,description):
+    def __init__(self,ques_id,description):
         self.ques_id = ques_id
-        self.option = option
         self.description= description
