6f5786f34d9c12832e5e0a7451b2cc9bc6ab9020
Nilesh Jha <nileshjha412@gmail.com>
2021-11-27 00:46:06 +0530

    Remove deepcopy bug

--- resources/response.py
+++ resources/response.py
@@ -2,6 +2,7 @@ from flask_restful import Resource
 from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
+import copy
 from models.form import FormModel
 from models.user import UserModel
 from models.question import QuestionModel
@@ -32,8 +33,8 @@ def get_form_structure(_id,_title):
             items[i]['statements']=get_statements(items[i]['questionId'])
     for x in QuestionModel.query.filter_by(form_id = form.id):
         print(x.question)
-    print(items)
-    print(form.title)
+    #print(items)
+    #print(form.title)
     formPayload = {
         "title" : _title,
         "formId" : form.id,
@@ -84,16 +85,17 @@ class GetResponse(Resource):
     @jwt_required()
     def get(self, _id, _title):
         formstruc = get_form_structure(_id, _title)
-        print(formstruc)
+        #print(formstruc)
         form = FormModel.query.filter_by(title = _title, creator_id = _id).first() 
         resObj = form.response
         users = set()
         for i in resObj:
             users.add(i.user_id)
         response = []
+        print(users)
         for i in users:
             responses = ResponseModel.resp_user(i,form.id)
-            temp = formstruc
+            temp = copy.deepcopy(formstruc)
             for question in temp['items']:
                 if question['type'] == 2:
                     question['answer'] = responses[question['questionId']].split(' $$$ ')
