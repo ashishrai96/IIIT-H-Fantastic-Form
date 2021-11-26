9a9892b90dbe869c9933b10b6d12760e91864107
Ashish Rai <ashishrai96@yahoo.com>
2021-11-26 19:05:24 +0530

    Cors issue resolved

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ from resources.question import QuestionList, Question
 from models.option import OptionModel
 from models.response import ResponseModel
 from resources.transcript import Transcipt
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
